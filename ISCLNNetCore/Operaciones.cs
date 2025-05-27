ebcf76afa6ba49fd7602662fea9799b8a7b1a0af
cesar <cesar.angeloff@nortedesarrollos.com>
2025-05-26 22:38:08 -0300

    Agregar archivos de proyecto.

---
+++ ISCLNNetCore/Operaciones.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ISCLNNetCore
+{
+    public class Operaciones
+    {
+        public int Suma_Igual_A_1()
+        {
+            int x = 1;
+            int z = 0;
+
+            return x + z; 
+        }
+    }
+}
---
+++ Test/OperacionesTest.cs
@@ -0,0 +1,31 @@
+﻿using ISCLNNetCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test
+{
+    public class OperacionesTest
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            int resultadoEsperado = 1;
+
+
+            Operaciones operaciones = new Operaciones();
+
+            // Act
+            var result = operaciones.Suma_Igual_A_1();
+
+            // Assert
+
+            Assert.Equal(resultadoEsperado, result);
+
+
+        }
+    }
+}
